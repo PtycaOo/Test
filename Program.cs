809660ce995d9c70c06587565ad350254dae5dba
PtycaOo <DenKuzia5155@gmail.com>
2023-07-25 21:08:41 +0300

    Метод Getmassive для получения массива от пользователя

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите слова для заполнения массива через запятую:\n Например: Мама,Папа,Сын");
+
+
+
+
+
+
+
+String[] GetMassiv(){
+String? s = Console.ReadLine();
+        String[] OriginMassiv = s.Split(',');
+    return OriginMassiv;
+}
+
+String[] Origin = GetMassiv();
