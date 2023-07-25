ece2a2234b33faef225ca4e434f52241ca2e2262
PtycaOo <DenKuzia5155@gmail.com>
2023-07-25 21:24:28 +0300

    Метод определяющий длинну нового массива

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿Console.WriteLine("Введите слова для заполнения массива через запятую:\n Например: Мама,Папа,Сын");
 
-
-
-
-
-
-
 String[] GetMassiv(){
 String? s = Console.ReadLine();
         String[] OriginMassiv = s.Split(',');
     return OriginMassiv;
+}
+
+int number_new_massive(String[] arr){
+    int Count = 0;
+for(int i = 0; i < arr.Length; i++){
+    if(arr[i].Length <= 3){
+        Count++;
+    }  
+}
+return Count;
 }
 
 String[] Origin = GetMassiv();
